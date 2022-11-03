4a505dfa094c6312e7a954cfe34c5f1e2a0f8b44
alemonacoo <ale.monaco@outlook.com>
2022-11-03 14:57:24 +0100

    Setting up main areas + Uploading images

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,13 +1,10 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-    <title>Laravel Comics</title>
-</head>
-<body>
-    prova
-</body>
-</html>
+@extends('layouts._main')
+
+@section('title')
+    DC Comics
+@endsection
+
+@section('main')
+    <div>QUESTO é IL CENTRO!!</div>
+    
+@endsection
