40ab5aa442b10c3b71d746ab7fa6b1f56108a415
alemonacoo <ale.monaco@outlook.com>
2022-11-03 16:38:53 +0100

    Footer Completed!

--- resources/views/partials/_footer.blade.php
+++ resources/views/partials/_footer.blade.php
@@ -1,1 +1,26 @@
-FOOTER
+<footer>
+    <div class="top-footer">
+        <div class="lists-container">
+            @include('partials._list-component')
+            @include('partials._list-component')
+            @include('partials._list-component')
+            @include('partials._list-component')
+
+        </div>
+        <img src="{{asset('/images/dc-logo-bg.png')}}" alt="dc footer logo">
+
+    </div>
+
+
+    <div class="bottom-footer">
+        <button>SIGN UP NOW!</button>
+        <div>
+            <h3>FOLLOW US</h3>
+            <img src="{{asset('images/footer-facebook.png')}}" alt="">
+            <img src="{{asset('images/footer-periscope.png')}}" alt="">
+            <img src="{{asset('images/footer-pinterest.png')}}" alt="">
+            <img src="{{asset('images/footer-twitter.png')}}" alt="">
+            <img src="{{asset('images/footer-youtube.png')}}" alt="">
+        </div>
+    </div>
+</footer>
---
+++ resources/views/partials/_list-component.blade.php
@@ -0,0 +1,11 @@
+<div class="list">
+    <ul>
+        <h3>Titolo lista</h3>
+        <li>Link</li>
+        <li>Link</li>
+        <li>Link</li>
+        <li>Link</li>
+        <li>Link</li>
+    </ul>
+
+</div>
