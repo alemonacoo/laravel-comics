46c01b7729a1a461fd56caded7f8cc8d676e9e75
alemonacoo <ale.monaco@outlook.com>
2022-11-03 15:21:47 +0100

    Header + Jumbotron completed

--- resources/views/partials/_header.blade.php
+++ resources/views/partials/_header.blade.php
@@ -1,1 +1,20 @@
-HEADER
+<header> 
+    <img src="{{ asset('images/dc-logo.png')}}" alt="dc-logo">
+    <nav>
+        <ul>
+            <li>CHARACTERS</li>
+            <li>COMICS</li>
+            <li>MOVIES</li>
+            <li>TV</li>
+            <li>GAMES</li>
+            <li>COLLECTIBLES</li>
+            <li>VIDEOS</li>
+            <li>FANS</li>
+            <li>NEWS</li>
+            <li>SHOP</li>
+        </ul>
+    </nav>
+  </header>
+<div class="jumbotron">
+    <img src="{{asset('images/jumbotron.jpg')}}" alt="">
+</div>
